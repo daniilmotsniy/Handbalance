925f6554c953bbca57214309294eb7f522f4feb0
Daniil Motsnyi <daniil_motsnyi@epam.com>
2021-07-31 12:25:30 +0300

    ec2

--- handbalance/handbalance/settings.py
+++ handbalance/handbalance/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ysx0(5flgitn8s@r*$)dh=l)@ve#vcwfc!kb%yn=_-0n8c+@4l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ec2-18-116-27-226.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
